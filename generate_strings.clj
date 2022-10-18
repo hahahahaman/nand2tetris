14aae989e3f48eaaec71161451f17fd8549d96ba
Ed Ye <edwardye@protonmail.com>
2022-10-18 04:26:05 -0400

    ALU passes the tests

--- generate_strings.clj
+++ generate_strings.clj
@@ -3,4 +3,8 @@
 (defn gen-str [n]
   (dotimes [x n]
     (println
-      (str "HalfAdder(a=in[" x "], b=c" x ", sum=out[" x "], carry=c" (inc x) ");"))))
+      ;; (str "HalfAdder(a=in[" x "], b=c" x ", sum=out[" x "], carry=c" (inc x) ");")
+      ;; (str "And(a=a[" x "], b=a[" x "], out=out[" (inc x) "]);")
+      ;; (str "Equals(a=a[" x "], b=b[" x "], out=o" x ");")
+      (str "And(a=andout" x ", b=o" (+ x 2) ", out=andout" (inc x) ");")
+      )))
