8444c9ce6b66346073c319c0db8a808f25f10648
Ed Ye <edwardye@protonmail.com>
2022-10-21 02:32:03 -0400

    Done chapter 3. Nice!

--- generate_strings.clj
+++ generate_strings.clj
@@ -2,9 +2,13 @@
 
 (defn gen-str [n]
   (dotimes [x n]
-    (println
-      ;; (str "HalfAdder(a=in[" x "], b=c" x ", sum=out[" x "], carry=c" (inc x) ");")
-      ;; (str "And(a=a[" x "], b=a[" x "], out=out[" (inc x) "]);")
-      ;; (str "Equals(a=a[" x "], b=b[" x "], out=o" x ");")
-      (str "And(a=andout" x ", b=o" (+ x 2) ", out=andout" (inc x) ");")
-      )))
+    ;; (println
+    ;;   ;; (str "HalfAdder(a=in[" x "], b=c" x ", sum=out[" x "], carry=c" (inc x) ");")
+    ;;   ;; (str "And(a=a[" x "], b=a[" x "], out=out[" (inc x) "]);")
+    ;;   ;; (str "Equals(a=a[" x "], b=b[" x "], out=o" x ");")
+    ;;   (str "Mux(a=PrevIn" x ", b=in[" x "], sel=load, out=NextIn" x ");"))
+    ;; (println (str "DFF(in=NextIn" x ", out=PrevIn" x ", out=out[" x "]);"))
+    ;; (println (str "Register(in=in, load=r" x ", out=RegVal" x ");"));
+    (println (str "RAM8(in=in, load=r" x ", address[0]=address[0], address[1]=address[1], out=Ram" x ");"))
+    (println "")
+    ))
