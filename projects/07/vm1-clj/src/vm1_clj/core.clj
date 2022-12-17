aeb3eac72971cb47131f4a456b1136599ba2ce33
Ed Ye <edwardye@protonmail.com>
2022-12-16 23:31:35 -0500

    stack arithmetic working for chap 7 project

--- projects/07/vm1-clj/src/vm1_clj/core.clj
+++ projects/07/vm1-clj/src/vm1_clj/core.clj
@@ -5,13 +5,87 @@
   (:gen-class))
 
 
-;; TODO use java/clojure built-in checking system for directory or files
-;; https://clojure-doc.org/articles/cookbooks/files_and_directories/
-;; https://docs.oracle.com/javase/7/docs/api/java/io/File.html
+(def symbol-address-table
+  {"SP" 0
+   "LCL" 1
+   "ARG" 2
+   "THIS" 3
+   "THAT" 4
+   "R0" 0
+   "R1" 1
+   "R2" 2
+   "R3" 3
+   "R4" 4
+   "R5" 5
+   "R6" 6
+   "R7" 7
+   "R8" 8
+   "R9" 9
+   "R10" 10
+   "R11" 11
+   "R12" 12
+   "R13" 13
+   "R14" 14
+   "R15" 15
+   "SCREEN" 16384
+   "KBD" 24576})
 
-;; https://rosettacode.org/wiki/Extract_file_extension#Clojure
-(defn get-file-extension [path]
+(defn get-file-extension
+  "File extension from the path string. Return value includes the period: .txt
+  https://rosettacode.org/wiki/Extract_file_extension#Clojure"
+  [path]
   (second (re-find #"(\.[a-zA-Z0-9]+)$" path)))
+
+(def inc-stack-pointer
+  (s/join
+   "\n"
+   ["@SP"
+    "M=M+1"]))
+
+(def dec-stack-pointer
+  (s/join
+   "\n"
+   ["@SP"
+    "M=M-1"]))
+
+(defn inc-pointer [p-str]
+  (s/join
+   "\n"
+   [(str "@" p-str)
+    "M=M+1"]))
+
+(defn dec-pointer [p-str]
+  (s/join
+   "\n"
+   [(str "@" p-str)
+    "M=M-1"]))
+
+(def D=SP ;; D register = value at address pointed to by SP
+  (s/join
+   "\n"
+   ["@SP"
+    "A=M"
+    "D=M"]))
+
+(def SP=D ;; value at address pointed to by SP = D register
+  (s/join
+   "\n"
+   ["@SP"
+    "A=M"
+    "M=D"]))
+
+(def SP=SP+D
+  (s/join
+   "\n"
+   ["@SP"
+    "A=M"
+    "M=M+D"]))
+
+(defn D=v [v]
+  (s/join
+   "\n"
+   [(str "@" v)
+    "D=A"]))
 
 (defn -main [& args]
   (cond
@@ -21,19 +95,331 @@
     (let [path (io/file (s/trim (nth args 0)))
           exists? (.exists path)
           dir? (.isDirectory path)
-          files (if dir? (vec (file-seq path)) [path])]
+          files (if dir? (vec (file-seq path)) [path])
+          !stack (atom [])]
       (if (not exists?)
         (println "ERROR:" path " does not exist!")
 
         (doseq [file files]
           (let [file? (.isFile file)
-                filename (.getName file)
-                extension (get-file-extension (.getName file))]
+                extension (get-file-extension (.getName file))
+                !label-counter (atom 0)
+                vm-label "VMLABEL"
+                new-label! (fn []
+                             (let [symbol (str vm-label @!label-counter)
+                                   label (str "(" symbol ")")]
+                               (swap! !label-counter inc)
+                               {:label label
+                                :symbol symbol}))
+
+
+                ;; set sp 256,        // stack pointer
+                ;; set local 300,     // base address of the local segment
+                ;; set argument 400,  // base address of the argument segment
+                ;; set this 3000,     // base address of the this segment
+                ;; set that 3010,     // base address of the that segment
+                !out (atom
+                      ;; init stack pointer: M[0] = 256
+                      ["@256" ;; SP 256
+                       "D=A"
+                       "@SP" ;; stack pointer, at address 0. set A=0
+                       "M=D" ;; M[0]=256
+
+                       "@300" ;; LCL 300
+                       "D=A"
+                       "@LCL"
+                       "M=D"
+
+                       "@400" ;; ARG 400
+                       "D=A"
+                       "@ARG"
+                       "M=D"
+
+                       "@3000" ;; THIS 3000
+                       "D=A"
+                       "@THIS"
+                       "M=D"
+
+                       "@3010" ;; THAT 4000
+                       "D=A"
+                       "@THAT"
+                       "M=D"]
+
+                      )]
             (if file?
               (if (= extension ".vm")
-                (do
-                  ;; parse
-                  )
+                (let [lines (with-open [rdr (io/reader file)]
+                              (doall (mapv s/trim (line-seq rdr))))]
+                  (loop [i 0]
+                    (when (< i (count lines))
+                      (let [line (nth lines i)
+                            [op seg idx-str] (s/split line #" ")
+                            idx (when idx-str (parse-long idx-str))]
+
+                        ;; stack arithmetic commands
+                        ;; memory access commands
+                        (condp = op
+                          "add"
+                          (swap! !out conj
+
+                                 ;; "@SP" ;; A = 0            ;; M[0] = 258
+                                 ;; "M=M-1" ;; M[0] = M[0] -1 ;; M[0] = 257
+                                 dec-stack-pointer
+
+                                 ;; "A=M" ;; A = M[0]         ;; A = 257
+                                 ;; "D=M" ;; D = M[0]         ;; D = M[257]
+                                 D=SP
+
+                                 ;; "@SP" ;; A = 0
+                                 ;; "M=M-1" ;; M[0] = M[0]-1  ;; M[0] = 256
+                                 dec-stack-pointer
+
+                                 "A=M" ;; A = M[0]         ;; A = 256
+                                 "M=M+D" ;; M[256]=M[256]+M[257]
+
+                                 ;; "@SP"
+                                 ;; "M=M+1" ;; M[0] = 257
+                                 inc-stack-pointer)
+
+                          "sub"
+                          (swap! !out conj
+                                 dec-stack-pointer
+
+                                 D=SP
+
+                                 dec-stack-pointer
+
+                                 "A=M"
+                                 "M=M-D"
+
+                                 inc-stack-pointer)
+
+                          "neg"
+                          (swap! !out conj
+                                 dec-stack-pointer
+
+                                 "A=M"
+                                 "M=-M"
+
+                                 inc-stack-pointer)
+
+                          "eq"
+                          ;; -1 is true, 0 is false
+                          (let [{cond-sym :symbol cond-label :label} (new-label!)
+                                {end-sym :symbol end-label :label} (new-label!)]
+                            (swap! !out conj
+                                   ;; where do i jump
+                                   ;; i keep a symbol string with a incrementing
+                                   ;; suffix
+                                   ;; (LABEL1)
+
+                                   dec-stack-pointer
+
+                                   D=SP
+
+                                   dec-stack-pointer
+
+                                   "A=M" ;; set A to the SP address
+                                   "D=M-D"
+                                   (str "@" cond-sym)
+                                   "D;JEQ" ;; jump to the cond-label
+
+                                   ;; false, set M[SP]=0
+                                   "@SP"
+                                   "A=M"
+                                   "M=0"
+                                   (str "@" end-sym)
+                                   "0;JMP"
+
+                                   ;; set SP to -1
+                                   cond-label
+                                   "@SP"
+                                   "A=M"
+                                   "M=-1"
+
+                                   end-label
+                                   inc-stack-pointer))
+
+                          "gt"
+                          (let [{cond-sym :symbol cond-label :label} (new-label!)
+                                {end-sym :symbol end-label :label} (new-label!)]
+                            (swap! !out conj
+                                   ;; where do i jump
+                                   ;; i keep a symbol string with a incrementing
+                                   ;; suffix
+                                   ;; (LABEL1)
+
+                                   dec-stack-pointer
+
+                                   D=SP
+
+                                   dec-stack-pointer
+
+                                   "A=M" ;; set A to the SP address
+                                   "D=M-D"
+                                   (str "@" cond-sym)
+                                   "D;JGT" ;; jump to the cond-label
+
+                                   ;; false, set M[SP]=0
+                                   "@SP"
+                                   "A=M"
+                                   "M=0"
+                                   (str "@" end-sym)
+                                   "0;JMP"
+
+                                   ;; set SP to -1
+                                   cond-label
+                                   "@SP"
+                                   "A=M"
+                                   "M=-1"
+
+                                   end-label
+                                   inc-stack-pointer))
+
+                          "lt"
+                          (let [{cond-sym :symbol cond-label :label} (new-label!)
+                                {end-sym :symbol end-label :label} (new-label!)]
+                            (swap! !out conj
+                                   ;; where do i jump
+                                   ;; i keep a symbol string with a incrementing
+                                   ;; suffix
+                                   ;; (LABEL1)
+
+                                   dec-stack-pointer
+
+                                   D=SP
+
+                                   dec-stack-pointer
+
+                                   "A=M" ;; set A to the SP address
+                                   "D=M-D"
+                                   (str "@" cond-sym)
+                                   "D;JLT" ;; jump to the cond-label
+
+                                   ;; false, set M[SP]=0
+                                   "@SP"
+                                   "A=M"
+                                   "M=0"
+                                   (str "@" end-sym)
+                                   "0;JMP"
+
+                                   ;; set SP to -1
+                                   cond-label
+                                   "@SP"
+                                   "A=M"
+                                   "M=-1"
+
+                                   end-label
+                                   inc-stack-pointer))
+
+                          "and"
+                          (swap! !out conj
+                                 dec-stack-pointer
+
+                                 D=SP
+
+                                 dec-stack-pointer
+
+                                 "A=M"
+                                 "M=M&D"
+
+
+                                 inc-stack-pointer)
+
+                          "or"
+                          (swap! !out conj
+                                 dec-stack-pointer
+
+                                 D=SP
+
+                                 dec-stack-pointer
+
+                                 "A=M"
+                                 "M=M|D"
+
+                                 inc-stack-pointer)
+
+                          "not"
+                          (swap! !out conj
+                                 dec-stack-pointer
+
+                                 "A=M"
+                                 "M=!M"
+
+                                 inc-stack-pointer)
+
+
+                          ;; 0. You have already handled the constant segment.
+                          ;;
+                          ;; 1. Next, handle the segments local, argument, this,
+                          ;; and that.
+                          ;;
+                          ;; 2. Next, handle the pointer and temp segments, in
+                          ;; particular allowing modiﬁca- tion of the bases of
+                          ;; the this and that segments.
+                          ;;
+                          ;; 3. Finally, handle the static segment.
+
+                          ;; push s i; push the value of s[i] onto the stack
+                          ;; pop  s i; pop top of stack and store into s[i]
+                          "push"
+                          (condp = seg
+                            "constant"
+                            (swap! !out conj
+                                   ;; put constant into D-register
+                                   (str "@" idx)
+                                   "D=A"
+
+                                   ;; set value at the top of the stack to
+                                   ;; the constant
+                                   "@SP"
+                                   "A=M"
+                                   "M=D"
+
+                                   ;; increment stack pointer
+                                   "@SP"
+                                   "M=M+1")
+
+                            "local"
+                            (swap! !out conj)
+
+                            "argument"
+                            (swap! !out conj)
+
+                            "this"
+                            (swap! !out conj)
+
+                            "that"
+                            (swap! !out conj)
+
+                            (println "ERROR: No matching push SEGMENT" seg))
+
+                          "pop"
+                          (condp = seg
+
+                            (println "ERROR: No matching pop SEGMENT" seg))
+
+                          ;; comment
+                          "//" nil
+
+                          ;; empty
+                          "" nil
+
+                          (println "Line" (inc i)
+                                   ". ERROR: No matching operation"
+                                   op)))
+                      (recur (inc i))))
+                  (let [filepath (.getPath file)
+                        asm-filename (str (subs filepath 0 (- (count filepath)
+                                                              (count ".vm")))
+                                          ".asm")]
+                    (println "Writing to:" asm-filename)
+                    (with-open [w (io/writer asm-filename :append false)]
+                      (doseq [s @!out]
+                        (.write w (str s "\n"))
+                        ;; (println s)
+                        ))))
+
                 (println "Skipping" path "not a .vm file."))
               (println "Skipping" path "not a file."))))))
 
