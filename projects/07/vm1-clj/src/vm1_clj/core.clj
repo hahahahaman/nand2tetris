5587d2388355232a956b7e2c9aab9043e511ceb0
Ed Ye <edwardye@protonmail.com>
2022-12-19 02:26:02 -0500

    comment unused. split string using \s white regex

--- projects/07/vm1-clj/src/vm1_clj/core.clj
+++ projects/07/vm1-clj/src/vm1_clj/core.clj
@@ -5,30 +5,31 @@
   (:gen-class))
 
 
-(def symbol-address-table
-  {"SP" 0
-   "LCL" 1
-   "ARG" 2
-   "THIS" 3
-   "THAT" 4
-   "R0" 0
-   "R1" 1
-   "R2" 2
-   "R3" 3
-   "R4" 4
-   "R5" 5
-   "R6" 6
-   "R7" 7
-   "R8" 8
-   "R9" 9
-   "R10" 10
-   "R11" 11
-   "R12" 12
-   "R13" 13
-   "R14" 14
-   "R15" 15
-   "SCREEN" 16384
-   "KBD" 24576})
+(comment
+  (def symbol-address-table
+   {"SP" 0
+    "LCL" 1
+    "ARG" 2
+    "THIS" 3
+    "THAT" 4
+    "R0" 0
+    "R1" 1
+    "R2" 2
+    "R3" 3
+    "R4" 4
+    "R5" 5
+    "R6" 6
+    "R7" 7
+    "R8" 8
+    "R9" 9
+    "R10" 10
+    "R11" 11
+    "R12" 12
+    "R13" 13
+    "R14" 14
+    "R15" 15
+    "SCREEN" 16384
+    "KBD" 24576}))
 
 (defn get-file-extension
   "File extension from the path string. Return value includes the period: .txt
@@ -160,7 +161,7 @@
                   (loop [i 0]
                     (when (< i (count lines))
                       (let [line (nth lines i)
-                            [op seg idx-str] (s/split line #" ")
+                            [op seg idx-str] (s/split line #"\s")
                             idx (when idx-str (parse-long idx-str))]
 
                         ;; stack arithmetic commands
